f3dfc62b715dab621a104b38f0cff8d5f683acfa
Gonzalo Garcia <ggarciah@eafit.edu.co>
2020-03-30 08:36:16 -0400

    main2

--- Main2.java
+++ Main2.java
@@ -8,8 +8,12 @@ class Main2 {
     public static void main2(String args[]) throws IOException{
         FactoryProvider provider = new FactoryProvider();
         AbstractFactory factoryFacturas = provider.getFactory("facturaFactory");
+        AbstractFactory factoryClientes = provider.getFactory("clienteFactory");
+        AbstractFactory factoryItems = provider.getFactory("itemFactory");
 
         FacturaDaoImpl facturaDao = new FacturaDaoImpl();
+        ClienteDaoImpl clienteDao = new ClienteDaoImpl();
+        ItemDaoImpl itemDao = new ItemDaoImpl();
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -45,13 +49,72 @@ class Main2 {
 
         }else{
             System.out.println("Hola Administrador, qué deseas hacer?");
+            System.out.println("Crear una factura nueva. (c)");
             System.out.println("Ver historial de facturas. (r)");
             System.out.println("Actualizar una factura. (u)");
             System.out.println("Borrar una factura. (d)");
 
             String resA = br.readLine();
 
-            if(resA.equals("r")){
+            if(resA.equals("c")){
+
+                System.out.println("Ha seleccionado crear factura nueva");
+                System.out.println("Qué tipo de factura desea crear, factura Vencida o Pagada? (v/p)");
+
+                String tipoFactura = br.readLine();
+
+                if(tipoFactura.equals("v")){
+
+                    Factura newFactura = factoryFacturas.getFactura("facturaVencida")
+
+                }else{
+
+                    System.out.println("Por defecto, factura Pagada.");
+                    Factura newFactura = factoryFacturas.getFactura("facturaPagada");
+
+                }
+
+                /*
+                private int nroFactura;
+
+                private String fechaFactura;
+                            private Cliente cliente;
+                private double totalFactura;
+                private String estado;
+                    private ArrayList <Item> items;
+                */
+
+                Random  rnd = new Random();
+                int nroFactura = (int) (rnd.nextDouble() * 100 + 20);
+
+                System.out.println("ingrese la fecha de la factura:");
+                String fecha = br.readLine();
+
+                System.out.println("ingrese el total:");
+                Double total = Double.parseDouble( br.readLine() );
+
+                System.out.println("ingrese el estado:");
+                String estado = br.readLine();
+                
+                System.out.println("Ahora, su cliente. El es de tipo Mayor o Menor? (m/n)");
+                String tipoCliente = br.readLine();
+                if(tipoCliente.equals("m") ){
+                    Cliente cliente = factoryClientes.getCliente("clienteMayor");
+                }else{
+                    Cliente cliente = factoryClientes.getCliente("clienteMenor");
+                }
+
+                System.out.println("Ahora, empecemos a agregar los items que su cliente ha comprado:");
+
+                while(true){
+                    System.out.println("A continuación, se mostrarán los items en venta:");
+                    itemDao.printItem();
+
+                    System.out.println("\n Escriba el id del item que desea incluir en la factura");
+                    
+                }
+
+            }else if(resA.equals("r")){
 
                 System.out.println("Imprimiendo facturas...");
                 System.out.println();
@@ -61,20 +124,6 @@ class Main2 {
                 System.out.println();
 
             }else if(resA.equals("u")){
-                /*
-                private int nroFactura;
-
-                private String fechaFactura;
-                private Cliente cliente;
-                private double totalFactura;
-                private String estado;
-                private ArrayList <Item> items;
-                */
-
-                /*
-                Random  rnd = new Random();
-                int nroFactura = (int) (rnd.nextDouble() * 100 + 20);
-                */
                 facturaDao.printFacturas();
                 System.out.println();
                 System.out.println("Perfecto empecemos, ingresa el Nro de la factura a editar:");
@@ -127,52 +176,24 @@ class Main2 {
 
                 facturaDao.updateFactura(nroFactura, newFactura);
 
+            }else if(resA.equals("d")){
+
+                facturaDao.printFacturas();
+                System.out.println();
+
+                System.out.println("Por favor ingresa el nro de factura que deseas eliminar.");
+
+                int nroFactura = Integer.parseInt( br.readLine() );
+                facturaDao.delFactura(nroFactura);
+
+                System.out.println("Factura eliminada exitosamente.\n");
+
             }else{
                 System.out.println("Has sido detectado como Hacker, el sistema se apagará enseguida.");
             }
         }
 
-        /*
-        ClienteMenor juan = new ClienteMenor();
-        juan.saludar();
-
-        juan.setId(1);
-        juan.setNombre("Juan");
-        juan.setApellido("García");
-        juan.setGenero("Masculino");
-        juan.setFechaNacimiento("23/11/1999");
-        juan.setEstadocivil("Soltero");
-
-        ClienteDaoImpl clienteDao = new ClienteDaoImpl();
-        clienteDao.addCliente(juan);
-        clienteDao.printClientes();
-
-        ClienteMayor pedro = new ClienteMayor();
-        pedro.saludar();
         
-        
-        
-        FactoryProvider provider = new FactoryProvider();
-
-        AbstractFactory factory = provider.getFactory("clienteFactory");
-        Cliente cliente = factory.getCliente("clienteMenor");
-        
-        
-        cliente.saludar();
-
-
-        factory = provider.getFactory("facturaFactory");
-        Factura factura =factory.getFactura("facturaVencida");
-        factura.validarEstado();
-
-        factory = provider.getFactory("itemFactory");
-        Item item = factory.getItem("itemLocal");
-        item.consultarEstado();
-
-        factory = provider.getFactory("tipoFactory");
-        TipoItem tipoItem = factory.getTypeItem("tipoIlegal");
-        tipoItem.validarLegaliad();
-        */
 
     }
 }
